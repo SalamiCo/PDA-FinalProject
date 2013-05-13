65a2d2c0cbb8e057b3a13d6b66246920d7143348
Daniel Escoz <darkhogg@gmail.com>
2013-05-13 17:41:54 +0200

    Añadidos unos pocos ficheros iniciales al proyecto
    * Fichero `.gitignore` para ignorar los ficheros compilados
    * Fichero `main.sh`, script bash que compila y ejecuta el proyecto
    * Fichero `src/main.plf` casi vacío, con un objetivo `main`
    * Frolog y el preludio frolog en la crpeta `lib`

---
+++ main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+declare -r MAIN="main"
+declare -r EXTPL=".pl"
+declare -r EXTPLF=".plf"
+declare -r PROLOG="swipl"
+declare -r SRCDIR="src"
+declare -r BINDIR="bin"
+declare -r LIBDIR="lib"
+declare -r FROLOG="$LIBDIR/frolog.pl"
+
+cd "$(dirname "$0")"
+
+echo -e "\033[mCompiling \033[33m$EXTPLF\033[m files to \033[33m$EXTPL\033[m..."
+for FILE in $(find src -name "*.plf" | sort); do
+  if [ -f "$FILE" ]; then
+    OUT="${FILE%.*}$EXTPL"
+    DEST="$BINDIR/${OUT#*/}"
+
+    if [ ! -e "$DEST" ] || [ "$DEST" -ot "$FILE" ]; then
+      echo -ne "\033[m  Compiling \033[34m$FILE\033[m to \033[34m$DEST\033[m... "
+      echo "consult('$FROLOG'). expand('${FILE%.*}')." | "$PROLOG" >/dev/null 2>&1
+
+      if [ -e "$OUT" ]; then
+        mkdir -p "$(dirname "$DEST")"
+        mv "$OUT" "$DEST"
+        echo -e "\033[32mok"
+      else
+        echo -e "\033[31mfail"
+      fi
+    fi
+  fi
+done
+
+echo -e "\033[mRunning the \033[33mmain.pl\033[m file..."
+"$PROLOG" --quiet --nodebug -s "$BINDIR/$MAIN$EXTPL" -g "main, halt"
